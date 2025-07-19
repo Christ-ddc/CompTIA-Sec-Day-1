38c16daf80aecd7e5f3ff1ed5604dbb30df4151c
Christian <christiandumigpi.cd@gmail.com>
2025-07-19 16:16:28 +0800

    Add files via upload

---
+++ Untitled2.ps1
@@ -0,0 +1,9 @@
+﻿CIA: confidentiality, Encrypt data traffic:
+@coreBABA:
+config t 
+username admin privilege 15 secret pass
+ip domain-name rivansec.com
+crypto key generate rsa
+line vty 0 14
+    login local
+end
